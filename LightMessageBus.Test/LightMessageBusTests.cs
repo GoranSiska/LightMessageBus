0838288dd360a7907ab9dabd7de985499d29c78c
Goran Šiška <GoranSiska@users.noreply.github.com>
2014-08-21 21:11:58 +0200

    added static access to default MessageBus, renaming

---
+++ LightMessageBus.Test/LightMessageBusTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace LightMessageBus.Test
+{
+    [TestFixture]
+    public class LightMessageBusTests
+    {
+        [Test]
+        public void Default_ReturnsSingleton()
+        {
+            Assert.IsNotNull(LightMessageBus.Default);
+        }
+    }
+}
---
+++ LightMessageBus/LightMessageBus.cs
@@ -0,0 +1,16 @@
+﻿namespace LightMessageBus
+{
+    /// <summary>
+    /// Light-weight message bus
+    /// </summary>
+    /// <remarks>
+    /// Used to enable communication between objects without direct references.
+    /// </remarks>
+    public class LightMessageBus
+    {
+        public static LightMessageBus Default
+        {
+            get { return new LightMessageBus(); }
+        }
+    }
+}
